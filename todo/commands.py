a04a8b59d5ce740a2286c0082d3606b18110ee0a
nikaxzzl <knika150409@yandex.ru>
2026-06-16 18:58:44 +0300

    feat: add 'list' command

---
+++ todo/commands.py
@@ -0,0 +1,8 @@
+def list_tasks():
+    tasks = load_tasks()
+    if not tasks:
+        print("Список пуст")
+        return
+    for t in tasks:
+        mark = "[x]" if t["done"] else "[ ]"
+        print(f"{mark} {t['id']:>3}. {t['title']}")
