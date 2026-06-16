919794f460f76289214df7af4dd6f0c4de999d0f
nikaxzzl <knika150409@yandex.ru>
2026-06-16 19:31:37 +0300

    feat: add 'list' command (#3)
    ## Что сделано
    Добавил команду `list` для отображения всех задач в консоли. Команда выводит:
    1. ID задачи
    2.маркер выполнения (`[x]` — выполнена, `[ ]` — нет)
    3.заголовок задачи.
    
    ## Как тестировать
    python main.py add "купить хлеб"
    python main.py add "позвонить маме"
    python main.py list

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import sys
-from todo.commands import add_task
+from todo.commands import add_task, list_tasks
 
 def main():
     if len(sys.argv) < 2:
         print("Usage: python main.py <command> [args]")
         return
+
     cmd = sys.argv[1]
+
     if cmd == "add":
         if len(sys.argv) < 3:
             print("Need title: python main.py add 'купить хлеб'")
             return
         add_task(sys.argv[2])
+    elif cmd == "list":
+        list_tasks()
     else:
         print(f"Unknown command: {cmd}")
-
-if __name__ == "__main__":
-    main()
+        
