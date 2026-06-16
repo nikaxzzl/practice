daede4f026e0c6845c5f2bbee2f3d1b47a744860
nikaxzzl <knika150409@yandex.ru>
2026-06-16 19:43:39 +0300

    feat: add 'done' and 'remove' commands

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
-from todo.commands import add_task, list_tasks
-
+from todo.commands import add_task, list_tasks, mark_done, remove_task
 def main():
     if len(sys.argv) < 2:
         print("Usage: python main.py <command> [args]")
@@ -15,6 +14,10 @@ def main():
         add_task(sys.argv[2])
     elif cmd == "list":
         list_tasks()
+    elif cmd == "done":
+        mark_done(int(sys.argv[2]))
+    elif cmd == "remove":
+        remove_task(int(sys.argv[2]))
     else:
         print(f"Unknown command: {cmd}")
         
